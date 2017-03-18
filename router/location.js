986da4acc3c9441228ba00391418ab007054511e
Jimmy <jimmy.tieu@mail.utoronto.ca>
2017-03-18 15:23:04 -0400

    locatoin

--- router/location.js
+++ router/location.js
@@ -2,11 +2,25 @@
 const express = require('express');
 const router = express.Router();
 
+const Location = require('../structures/location');
+
+router.post('/create',async (req,res)=>{
+    //call location request end points
+    let location = new Location();
+    console.log(req.body);
+    await location.create(req.body);
+    
+    //return id to phone so it knows whats the id
+    res.json(location.id);
+})
 
 router.use('/:id/profile', (req,res)=>{
     //call user request end points
     let id = req.params.id;
-    profile(id,res)
+    let location = new Location();
+	await location.pull();
+	
+	res.json(location);
 })
 
 
---
+++ structures/location.js
@@ -0,0 +1,48 @@
+//Location class constructor
+var locationSchema = require('../schemas/locationSchema');
+
+//declare new class location
+class Location {
+    
+    //constructor, runs and creates internal properties
+    constructor(id) {
+        if(id) this.id = id;
+    }
+
+    pull(){
+        return new Promise(resolve=>{
+            locationSchema.findOne({_id: this.id}).exec().then(location=>{
+                this.id = location._id
+                this.ownerID = location.ownerID;
+                this.address = location.address;
+				this.rating = location.rating;
+				this.price = location.price;
+				this.capacity = location.capacity;
+				this.availability = location.availability;
+				this.bookings = location.bookings;
+                resolve(0);
+            })
+        })
+    }
+
+    create(profile){
+        return new Promise(resolve=>{
+            let location = new locationSchema(profile);
+            location.save((err,location)=>{
+                if(err)console.log(err);
+                this.id = location._id
+                this.ownerID = location.ownerID;
+                this.address = location.address;
+				this.rating = location.rating;
+				this.price = location.price;
+				this.capacity = location.capacity;
+				this.availability = location.availability;
+				this.bookings = location.bookings;
+                resolve();
+            });
+        })
+    }
+
+}
+
+module.exports = Location;
