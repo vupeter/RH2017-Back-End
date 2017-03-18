a33a86eef22637819e83de37cbb9094d0eebe12c
Peter Vu <me@petervu.ca>
2017-03-18 14:28:30 -0400

    more typo

--- router/user.js
+++ router/user.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../../structures/user');
+const User = require('../structures/user');
 
 
 router.post('/create',async (req,res)=>{
