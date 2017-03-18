7dd5780ab2f612c45fab84dd28ea0181fd15937d
Peter Vu <me@petervu.ca>
2017-03-18 16:19:40 -0400

    port change

--- api.js
+++ api.js
@@ -16,7 +16,7 @@ module.exports = {
         app.use(require('./router'));
 
         //Make the app start listening
-        app.listen(3000, function() {
+        app.listen(13125, function() {
             console.log('Listening on port 3000...');
         })
     }
