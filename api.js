5b6103f089d487176d2444d46315b5e2d66e04ed
Peter Vu <me@petervu.ca>
2017-04-09 21:48:08 -0400

    access control allow origin

--- api.js
+++ api.js
@@ -9,7 +9,7 @@ module.exports = {
         let app = express();
         //Body parse middleware
         var allowCrossDomain = function(req, res, next) {
-            res.header('Access-Control-Allow-Origin', 'cryndex.io');
+            res.header('Access-Control-Allow-Origin', '*');
             res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
             res.header('Access-Control-Allow-Headers', 'Content-Type');
 
