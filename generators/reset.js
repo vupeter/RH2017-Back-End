6561b892713697f53ef8fec5cc96e6b6f39c8ab6
Peter Vu <me@petervu.ca>
2017-04-10 22:31:08 -0400

    typo

--- generators/reset.js
+++ generators/reset.js
@@ -1,5 +1,5 @@
 let mongoose = require('mongoose');
-let Location = require('./schemas/locationSchema');
+let Location = require('../schemas/locationSchema');
 let User = require('./schemas/userSchema')
 
 mongoose.connect('mongodb://localhost/test');
